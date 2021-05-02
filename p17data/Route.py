862a77e863c56c9412d07c57c3cd0768b9e832a2
João Felipe Chiarelli Bourscheid <joao.felipe.c.b@gmail.com>
2021-05-02 17:46:00 -0300

    Initial version. Basic route test

---
+++ p17data/Route.py
@@ -0,0 +1,9 @@
+versions = {}
+
+versions['0.0.0.1.1'] = {}
+versions['0.0.0.1.1']['routes'] = {}
+versions['0.0.0.1.1']['routes']['/tags'] = 'p23control.Tags.list()'
+
+versions['0.0.0.1.1']['route'] = {}
+versions['0.0.0.1.1']['route']['routes'] = versions['0.0.0.1.1']['routes']
+
